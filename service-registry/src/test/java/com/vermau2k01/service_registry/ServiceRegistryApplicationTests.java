78ad42f00242748cef2f55fade69cd57221860cd
Utkarsh <vermau2k01@gmail.com>
2024-06-13 08:22:59 +0530

    Adding Eureka server

---
+++ service-registry/src/test/java/com/vermau2k01/service_registry/ServiceRegistryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vermau2k01.service_registry;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceRegistryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
